77c2c042ecf90f5752c4cc3e8e303ac690ab727b
rahathossainn <rahaaathossain@gmail.com>
2024-08-21 01:01:07 +0600

    done

--- alpha-clash.js
+++ alpha-clash.js
@@ -1,3 +1,40 @@
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log(event.key)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+
+    // checked match or not
+
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+        
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newlife = currentLife - 1;
+
+        currentLifeElement.innerText = newlife;
+        if(newlife === 0){
+            gameOver();
+        }
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
@@ -9,6 +46,12 @@ function continueGame(){
 function play(){
     // step 1: hide the home screen
     removeElementByID('home-screen')
+    removeElementByID('final-score')
     showElementById('play-ground')
     continueGame();
+}
+
+function gameOver(){
+    removeElementByID('play-ground');
+    showElementById('final-score');
 }
